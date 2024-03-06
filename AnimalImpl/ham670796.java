3e7ad3bcd75691ad0368e0fdac0af5c7cd83b234
hamil <ham670796@email.ccbcmd.edu>
2024-03-06 18:59:51 -0500

    This is my animal

---
+++ AnimalImpl/ham670796.java
@@ -0,0 +1,25 @@
+package SeeAndSay.AnimalImpl;
+
+import SeeAndSay.Animal;
+
+public class ham670796 extends Animal {
+    @Override
+    public String getAuthor() {
+        return "Corey Hamilton";
+    }
+
+    @Override
+    public String getAnimalName() {
+        return "Flamingo";
+    }
+
+    @Override
+    public String getSound() {
+        return "claw";
+    }
+
+    @Override
+    public String getDescription() {
+        return "."
+    }
+}
