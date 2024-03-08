6556dd42e0982042186d0e7096b8a528b0414741
fareedah <ow1131999@email.ccbcmd.edu>
2024-03-08 16:53:57 -0500

    imaginative hippo

---
+++ AnimalImpl/ow1131999.java
@@ -0,0 +1,25 @@
+package SeeAndSay.AnimalImpl;
+
+import SeeAndSay.Animallmpl.*;
+
+public class ow1131999 extends Animal {
+    @Override
+    public String getAuthor() {
+        return "Fareedah Owolabi";
+    }
+
+    @Override
+    public String getAnimalName() {
+        return "Imaginative Hippo";
+    }
+
+    @Override
+    public String getSound() {
+        return "loud wheezing sounds";
+    }
+
+    @Override
+    public String getDescription(){
+        return "The hippo is imaginative";
+    }
+}
