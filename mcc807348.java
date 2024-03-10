f4fe74365be735d980f84a8a7e661fe084913fe6
JordanMcCullum <mcc807348@email.ccbcmd.edu>
2024-03-10 09:57:55 -0400

    This is my anima

---
+++ mcc807348.java
@@ -0,0 +1,20 @@
+
+package SeeAndSay;
+
+public class mcc807348 extends Animal{
+
+    @Override
+    public String getAuthor() {
+        return " Jordan McCullum ";
+    }
+
+    @Override
+    public String getAnimalName() {
+        return "Creative Horse";
+    }
+
+    @Override
+    public String getSound() {
+        return "Blowwwwwwwwwwwwww";
+    }
+}
