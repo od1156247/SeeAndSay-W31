71cc8d0b956e3ae0e1da43fb914cd7663e35b7cc
Jennifer H Tobar <395ctob@gmail.com>
2024-03-14 21:49:51 -0400

    Add animal

---
+++ AnimalImpl/he1129401.java
@@ -0,0 +1,25 @@
+package SeeAndSay.AnimalImpl;
+
+import SeeAndSay.Animal;
+
+public class he1129401 extends Animal {
+    @Override
+    public String getAuthor() {
+        return "Jennifer H Tobar";
+    }
+
+    @Override
+    public String getAnimalName() {
+        return "Penguin";
+    }
+
+    @Override
+    public String getSound() {
+        return "Honk honk";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Very kind and polite";
+    }
+}
