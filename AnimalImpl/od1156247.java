c4dc12cb573af9b34f799f1dcf6388b8b7d643c9
kings <jsherry370@gmail.com>
2024-04-11 11:06:07 -0400

    This is my animal

---
+++ AnimalImpl/od1156247.java
@@ -0,0 +1,29 @@
+package SeeAndSay.AnimalImpl;
+
+import SeeAndSay.Animal;
+
+// Method that returns my username
+public class od1156247 extends Animal {
+    public String getAuthor(){
+        return "od1156247"; // Returns my username
+    }
+
+    //Method that Returns animals name
+    public String getAnimalName(){
+        return "Lion";    //Returns animals name
+    }
+
+    //Method that returns the sound that the animal makes
+    public String getSound(){
+        return "Rawrs"; //
+    }
+
+    // Method that returns a description of the lion
+
+    public String getDescription(){
+         return "Big and strong";
+    }
+
+}
+
+
